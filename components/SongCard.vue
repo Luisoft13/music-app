0cd56ecbd355692805e9d53280fd105b95c04508
Luis de la Cruz <gael130819@gmail.com>
2022-12-01 19:12:20 -0500

    feat: create SongCard component

---
+++ components/SongCard.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-card @click="$emit('play', chart)">
+    <v-img
+      :src="srcImage"
+      class="white--text align-end"
+      gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+      height="200px"
+    />
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    chart: {
+      type: Object,
+      default: null
+    },
+  },
+  computed: {
+    srcImage() {
+      return this.chart?.images?.coverart
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
