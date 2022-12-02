5b2404547c2cb8d73f1eba8066c829c52a59e364
Luis de la Cruz <gael130819@gmail.com>
2022-12-01 19:12:38 -0500

    feat: remove footer in default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -79,12 +79,6 @@
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
-    <v-footer
-      :absolute="!fixed"
-      app
-    >
-      <span>&copy; {{ new Date().getFullYear() }}</span>
-    </v-footer>
   </v-app>
 </template>
 
